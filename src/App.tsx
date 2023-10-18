8785fb7746c21adc08303f507010bfa86100b27f
Yuvraj Sirohi <yuvrajsirohi22@gmail.com>
2023-10-18 10:43:14 -0700

    import landing page adding it to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Amount from "../src/components/Amount/Amount"
+import LandingPage from './pages/LandingPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <Amount /> 
-      </header>
+    <LandingPage />
+
     </div>
   );
 }
