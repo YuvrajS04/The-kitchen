8878afe03817778c7381b0c10f0c77e0e8c3d327
Yuvraj Sirohi <yuvrajsirohi22@gmail.com>
2024-04-13 15:35:16 -0700

    refactor store setup to use react-persist for state persistence

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,13 +1,27 @@
+import {persistStore, persistReducer, PersistPartial} from "redux-persist";
+import storage from "redux-persist/lib/storage"
 import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "./menuSlice"
 
+const persistConfig= {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, menuReducer);
+
+
+
 const store = configureStore({
-    reducer: {
-        menu: menuReducer,
-    },
+        reducer: persistedReducer,
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootStateWithPersist = RootState & PersistPartial;
+export { store, persistStore };
 
+export type AppDispatch = typeof store.dispatch;
 export default store;
+
+
