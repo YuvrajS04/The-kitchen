37a17b17c1e53b417d579043f4330e534dbaa66b
Yuvraj Sirohi <yuvrajsirohi22@gmail.com>
2024-04-13 16:14:39 -0700

    add link to checkoutPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,12 @@
 import MenuList from "../components/MenuList/MenuList";
+import {Link} from "react-router-dom"
 
 function LandingPage() {
 
     return (
         <>
             <MenuList />
+            <Link to="/checkout"> Checkout</Link>
         </>
     )
 }
