41c6c95d25720f871acff780d294ddfe39846164
Yuvraj Sirohi <yuvrajsirohi22@gmail.com>
2024-04-13 16:13:46 -0700

    add link to landingPage

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,6 +1,7 @@
 import { RootState } from "../redux/store";
-import { addItem, removeItem } from "../redux/menuSlice"
-import { useSelector, useDispatch } from "react-redux"
+import { addItem, removeItem } from "../redux/menuSlice";
+import { useSelector, useDispatch } from "react-redux";
+import {Link} from "react-router-dom";
 
 function Checkout(){
     const menuItems = useSelector((state: RootState) => state.items);
@@ -15,6 +16,7 @@ function Checkout(){
     return (
         <>
         <div>
+            <Link to="/" >Return to Menu</Link>
             <p>
                 Checkout Page
             </p>
